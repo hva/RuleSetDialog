f6e0f4e9ece6a7c2096bda53e181216e9c54233c
Yauheni Khvaliuk <hvaluk@gmail.com>
2013-02-16 17:41:21 +0300

    repository access to files fixed

--- Repository/Repository.cs
+++ Repository/Repository.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Workflow.Activities.Rules;
+﻿using System.Workflow.Activities.Rules;
 using System.Workflow.ComponentModel.Serialization;
 using System.Xml;
 
@@ -8,22 +7,19 @@ namespace Repository
 	public class FileRepository
 	{
 		public RuleSet Load(string filename) {
-			XmlTextReader reader = new XmlTextReader(filename);
-			WorkflowMarkupSerializer serializer = new WorkflowMarkupSerializer();
-			object results = serializer.Deserialize(reader);
-			RuleSet ruleset = (RuleSet)results;
-
-			if (ruleset == null) {
-				Console.WriteLine("The rules file " + filename + " does not appear to contain a valid ruleset.");
+			using (XmlTextReader reader = new XmlTextReader(filename))
+			{
+				WorkflowMarkupSerializer serializer = new WorkflowMarkupSerializer();
+				return serializer.Deserialize(reader) as RuleSet;
 			}
-			return ruleset;
 		}
 
 		public void Save(RuleSet ruleset, string filename) {
-			XmlTextWriter writer = new XmlTextWriter(filename, null);
-			WorkflowMarkupSerializer serializer = new WorkflowMarkupSerializer();
-			serializer.Serialize(writer, ruleset);
-			Console.WriteLine("Wrote rules file: " + filename);
+			using (XmlTextWriter writer = new XmlTextWriter(filename, null))
+			{
+				WorkflowMarkupSerializer serializer = new WorkflowMarkupSerializer();
+				serializer.Serialize(writer, ruleset);
+			}
 		}
 	}
 }
