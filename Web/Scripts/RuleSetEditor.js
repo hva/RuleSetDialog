f7397b653fe068d2a21b234231248857c5e72c05
Yauheni Khvaliuk <hvaluk@gmail.com>
2013-02-17 13:54:54 +0300

    draw rule definition

--- Web/Scripts/RuleSetEditor.js
+++ Web/Scripts/RuleSetEditor.js
@@ -69,6 +69,9 @@
         $('#rulePriority').val(rule.Priority);
         $('#ruleReevaluation').val(rule.Reevaluation);
         $('#ruleActive').attr('checked', rule.Active);
+        $('#ruleCondition').text(rule.Condition);
+        $('#ruleThenActions').val(rule.ThenActions.join('\n'));
+        $('#ruleElseActions').val(rule.ElseActions.join('\n'));
     }
 
 })(jQuery);
